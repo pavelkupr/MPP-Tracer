5cfe91b365d4d81173f4afa3015090bf2157f62a
Pavel <pavelkupr88@gmail.com>
2018-09-14 19:52:40 +0300

    Basics added

---
+++ Tracer/Tracer/Tracer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+	public class Tracer : ITracer
+	{
+		public void StartTrace()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Stoptrace()
+		{
+			throw new NotImplementedException();
+		}
+
+		public TraceResult GetTraceResult()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
